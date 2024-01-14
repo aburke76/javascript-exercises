1b263029fa52cb4f42ef773e392f58c2ab0cf54c
aburke76 <88115621+aburke76@users.noreply.github.com>
2024-01-14 10:21:26 -0500

    temp converter, done without help

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToFahrenheit = function (degrees) {
+  let fahr = (degrees * 1.8 + 32);
+  return Math.round(fahr * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function (degrees) {
+  let cels = (degrees - 32) * (5 / 9);
+  return Math.round(cels * 10) / 10;
 };
 
 // Do not edit below this line
