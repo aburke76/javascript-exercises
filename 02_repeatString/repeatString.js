893b84e85014b53815b216267fde2d603f34ba25
aburke76 <88115621+aburke76@users.noreply.github.com>
2024-01-14 07:32:42 -0500

    helloWorld and repeatString exercises completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    let input = ""
+    if (num < 0) {
+        return 'ERROR'
+    }
+    for (let i = 0; i < num; i++){
+        input += string 
+    }
+    return input
 };
 
 // Do not edit below this line
