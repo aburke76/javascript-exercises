9eea32ea85041aaa52b6e13b0f323cae33c2ef8d
aburke76 <88115621+aburke76@users.noreply.github.com>
2024-01-14 11:24:31 -0500

    calculator, no help

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(...args) {
+  let sum = 0;
+  for (let arg of args) {
+    sum += arg
+  }
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let arraySum = 0;
+  array.forEach((element) => {
+    arraySum += element;
+  })
+  return arraySum;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let product = 1;
+  array.forEach((element) => {
+    product *= element;
+  })
+  return product;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  let result = 1;
+  for (let i = 0; i < b; i++){
+    result *= a
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+  for (let i = num; i <= num; i--) {
+    if (i === 0) { break };
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
