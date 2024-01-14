8fa9df035d6b9ada8e7d65f53b95ecc51fd46778
aburke76 <88115621+aburke76@users.noreply.github.com>
2024-01-14 07:59:29 -0500

    reverseString complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    let input = string.split("").reverse().join("")
+    return input;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+//hey, [0,1,2]
+//hey.length = 3
