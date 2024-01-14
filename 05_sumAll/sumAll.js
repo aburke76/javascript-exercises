e743b23dbfe114c4cfdd3ac0c2e2ab3235a5bb5e
aburke76 <88115621+aburke76@users.noreply.github.com>
2024-01-14 09:27:43 -0500

    sumAll, done without help

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let finalSum = 0;
+    if (typeof a !== "number" || typeof b !== "number") return "ERROR";
+    if (a < 0 || b < 0) return "ERROR";
+    if (a > b) {
+        for (let i = 0; i <= a; i++) {
+            finalSum += i
+        }
+    } else {
+        for (let i = 0; i <= b; i++) {
+            finalSum += i
+        }
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
