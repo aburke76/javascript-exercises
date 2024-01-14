94cf33778bc0bfc8f32d619186605c135226ea82
aburke76 <88115621+aburke76@users.noreply.github.com>
2024-01-14 08:31:44 -0500

    inc notes in my own words

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) { //example: array = [1,2,3], args = 2
+    updatedArray = [];
+    //empty array to return later
+    array.forEach((element) => {
+    //loops through each element, which are 1, 2, 3
+        if (!args.includes(element)) {
+            //if args does NOT include the current element,
+            updatedArray.push(element)
+            //add that element (push) to the end of updatedArray
+        }
+    })
+    return updatedArray;
 };
 
 // Do not edit below this line
