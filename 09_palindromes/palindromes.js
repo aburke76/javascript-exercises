173b5f0682a52ea066b706080e581dd615340422
aburke76 <alexlburke8771@gmail.com>
2024-01-14 21:31:52 -0500

    solution, with some help

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+    updatedString = string.toLowerCase().replace(/\W/g, '');
+    return updatedString.split("").reverse().join("") == updatedString;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
